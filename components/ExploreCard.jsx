ac2d5ce688f85c65eba053d63a2ef22de1ca4ff6
unknown <hughesgeorgeking@gmail.com>
2023-05-06 22:46:54 +0000

    changes

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -10,6 +10,7 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
     className={`relative ${
       active === id ? 'lg:flex-[3.5] flex-[10]' : 'lg:flex-[0.5] flex-[2]'
     } flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
+    onClick={() => handleClick(id)}
   >
     <img
       src={imgUrl}
@@ -35,6 +36,9 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
         <p className="font-normal text-[16px] leading-[20px] text-white uppercase">
           Enter the Metaverse
         </p>
+        <h2 className="mt-[24px] text-white font-semibold sm:text-[32px] text-[24px]">
+          {title}
+        </h2>
       </div>
     )}
   </motion.div>
